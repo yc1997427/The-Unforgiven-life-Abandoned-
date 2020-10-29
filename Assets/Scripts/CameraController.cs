82eb6379788f47255932f523e6cd8fbd9e829db3
yc997 <1084155272@qq.com>
2020-10-29 21:40:10 +0000

    update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,15 +8,34 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     private Vector3 offset;
 
+    public float mouseSensitivity=100f;
+
+    public Transform playerBody;
+    float xRotation=0f;
+
+
     void Start()
     {
     	offset=transform.position;
+        Cursor.lockState=CursorLockMode.Locked;
         
     }
+    void Update(){
+        float mouseX=Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
+        float mouseY=Input.GetAxis("Mouse Y")*mouseSensitivity*Time.deltaTime;
+
+        xRotation-=mouseY;
+        xRotation=Mathf.Clamp(xRotation,-90f,90f);
+        transform.localRotation=Quaternion.Euler(xRotation,0f,0f);
+        playerBody.Rotate(Vector3.up*mouseX);
+    }
+
 
     // Update is called once per frame
     void LateUpdate()
     {
     	transform.position=player.transform.position+offset;   
     }
+
+
 }
