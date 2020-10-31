ea0b123319a87c7dfeb2b2ab39a094ccf1fa7a3a
salmanriazsyed <72282430+salmanriazsyed@users.noreply.github.com>
2020-10-31 05:11:11 +0500

    Player Character & Basic Movement
    Player character added along with a few animations and basic movement. Camera is set to just follow the player for now, no mouse rotations yet.
    animations list:
    idle
    run
    run_fast
    backwards movement

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothspeed = 0.125f;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
